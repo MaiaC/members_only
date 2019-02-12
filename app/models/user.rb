e812ff1a6e20a57bed33a381eaf68f35dc8b80cd
Maia Coghlan <maiaccog@gmail.com>
2019-02-12 16:36:48 +0800

    implement sign in and remember cookies

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  attr_accessor :remember_cookie
+  
+  # before_create :generate_remember
+  
+  has_secure_password
+
+  def generate_remember
+    self.remember_cookie = SecureRandom.urlsafe_base64
+    update_attribute(:remember_token, Digest::SHA1.hexdigest(remember_cookie.to_s))
+  end
+
+  def authenticated?(r_c)
+    return false if remember_token.nil?
+    BCrypt::Password.new(remember_token)                                                     .is_password?(r_c)
+  end
+
+  def forget
+    update_attribute(:remember_token, nil)
+  end
+end
